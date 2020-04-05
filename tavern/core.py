8e6727ac6e9136a8de9e0ef20aef64db206d5ae3
Michael Boulton <michaelboulton@gmail.com>
2020-04-05 17:26:59 +0100

    Fix black

--- tavern/core.py
+++ tavern/core.py
@@ -9,7 +9,6 @@ import pytest
 
 from tavern.schemas.files import wrapfile
 from tavern.util.strict_util import StrictLevel
-from tavern.util.strict_util import StrictLevel
 from tavern.util.tincture import TinctureProvider
 
 from .plugins import get_expected, get_extra_sessions, get_request_type, get_verifiers
